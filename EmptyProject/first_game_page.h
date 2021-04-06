ca5c0c90da012280db703037ed6f2fa8f860a8c3
lyj310 <yoenjii310@gmail.com>
2021-04-07 00:36:39 +0900

    copy DirectXGamePractice_210405

---
+++ EmptyProject/first_game_page.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "page.h"
+#include "global.h"
+#include "player.h"
+
+class FirstGamePage : public Page
+{
+public:
+
+	FirstGamePage();
+	virtual ~FirstGamePage();
+
+	LPDIRECT3DTEXTURE9* backgroundTex;
+	LPDIRECT3DTEXTURE9* floorTex;
+	LPDIRECT3DTEXTURE9* maskTex;
+	LPD3DXSPRITE spr;
+	ID3DXFont* font;
+
+	Player* player;
+
+	bool isSpace;
+	bool isVisiting;
+
+	int visitingX;
+	int visitingY;
+
+	int myLand;
+
+
+	DWORD floorP[FLOOR_PIXEL];
+	DWORD maskP[FLOOR_PIXEL];
+
+	int map[FLOOR_PIXEL];
+	int virtualMap[FLOOR_PIXEL];
+
+
+
+	struct Point
+	{
+		int x;
+		int y;
+		Point(int x, int y)
+			: x(x), y(y) 
+		{}
+	};
+
+
+
+
+	void Bordering();
+	void FloodFill();
+	void GetLand();
+	void MapUpdate();
+	void PlayerMove(D3DXVECTOR2 dir);
+	void KeyInput();
+	void Update() override;
+	void Render() override;
+
+
+};
---
+++ EmptyProject/global.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "page_manager.h"
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+
+#define FLOOR_WIDTH 1024
+#define FLOOR_HEIGHT 720
+#define FLOOR_PIXEL FLOOR_WIDTH * FLOOR_HEIGHT
+
+#define GAME_X (WINDOW_WIDTH - FLOOR_WIDTH) / 2
+#define GAME_Y 0
+
+#define MAP_EMPTY 0
+#define MAP_EDGE 1
+#define MAP_VISITED 2
+#define MAP_VISITING 3
+#define MAP_TEMP 4
+
+
+#define FULL_LAND (FLOOR_HEIGHT - 2) * (FLOOR_WIDTH - 2)
+
+
+
+
+extern PageManager pageManager;
+
